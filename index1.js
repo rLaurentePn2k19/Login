1644829bb13e5d5fca1cbea8d34493c3592683a2
rLaurentePn2k19 <rangie.laurente@student.passerellesnumeriques.org>
2019-08-29 19:59:04 +0800

    Initial

---
+++ index1.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $("body").css("font-family","Varela Round");
+    $("h4").css({"text-align":"center","font-size":"26px"});
+    $(".modal-body").css("padding","30px");
+    $(".modal-login").css("width","320px");
+    $(".modal-content").css({"border-radius":"1px","border":"none"});
+    $(".modal-header").css({"position":"relative","justify-content":"center","background":"#f2f2f2"});
+    $(".modal-footer").css("background","#ff8080");
+    $("label").css({"font-weight":"normal","font-size":"13px"});
+    $(".form-control").css({"min-weight":"38px","border-radius":"20px"});
+    $(".btn").css({"min-weight":"38px","border-radius":"20px","min-width":"100px","background":"#3498db","line-height":"normal"});
+    $(".hint-text").css("text-align","center");
+    $(".close").css({"position":"absolute","top":"15px","right":"15px"});
+    $(".checkbox-inline").css("margin-top","12px");
+    $("input[type='checkbox']").css("margin-top","2px");
+    $(".trigger-btn").css({"display":"inline-block","margin":"100px auto"});
+    $("btn:hover").css("background","#248bd0");
+    $("btn:focus").css("background","#248bd0");
+});
+    
